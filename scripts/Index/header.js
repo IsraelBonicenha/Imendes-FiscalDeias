ef6065df8e00edee5f3e1da9721117c2b050ba6c
Israel Bonicenha <israelbonicenha2020@gmail.com>
2025-01-08 11:04:26 -0300

    feat: adiciona events para exibir o tolltip-header-question

--- scripts/Index/header.js
+++ scripts/Index/header.js
@@ -1,1 +1,10 @@
-// JavasScript
+function MudarOpacidadeElemento (elemento, opacit){
+    elemento.style.opacity = opacit;
+    elemento.style.transition = "opacity 0.7s ease";
+}
+
+const botaoInterrogacao = document.getElementById('header-circle-question');
+const tolltipDIV = document.querySelector('.tolltip-header-question');
+
+botaoInterrogacao.addEventListener('mouseover', () => MudarOpacidadeElemento(tolltipDIV, "100%"));
+botaoInterrogacao.addEventListener('mouseout', () => MudarOpacidadeElemento(tolltipDIV, "0%"));
