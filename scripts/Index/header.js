746c94e52f95937af5692760c1618d5ee4fc6ffb
Israel Bonicenha <israelbonicenha2020@gmail.com>
2025-01-09 15:33:52 -0300

    feat: adiciona div para escurecer a tela ao exibir tolltip e outras alterações

--- scripts/Index/header.js
+++ scripts/Index/header.js
@@ -1,10 +1,15 @@
-function MudarOpacidadeElemento (elemento, opacit){
+function MudarOpacidadeElemento (elemento, opacit, blockDIV, opacitBlockDIV){
     elemento.style.opacity = opacit;
     elemento.style.transition = "opacity 0.7s ease";
+
+
+    blockDIV.style.opacity = opacitBlockDIV
+    blockDIV.style.transition = "opacity 0.7s ease";
 }
 
 const botaoInterrogacao = document.getElementById('header-circle-question');
 const tolltipDIV = document.querySelector('.tolltip-header-question');
+const blockDIV = document.querySelector('.block-moment-tolltip')
 
-botaoInterrogacao.addEventListener('mouseover', () => MudarOpacidadeElemento(tolltipDIV, "100%"));
-botaoInterrogacao.addEventListener('mouseout', () => MudarOpacidadeElemento(tolltipDIV, "0%"));
+botaoInterrogacao.addEventListener('mouseover', () => MudarOpacidadeElemento(tolltipDIV, "100%", blockDIV, "80%"));
+botaoInterrogacao.addEventListener('mouseout', () => MudarOpacidadeElemento(tolltipDIV, "0%", blockDIV, "0%"));
