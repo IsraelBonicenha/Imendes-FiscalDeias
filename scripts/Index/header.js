775240e74bceff2e91a4d6cf4592ad80e0ab5806
Israel Bonicenha <israelbonicenha2020@gmail.com>
2025-01-10 09:33:22 -0300

    feach: corrige a posição do block-moment-tolltip e aletara a lógica do js para permitir interação com outros elementos da tela

--- scripts/Index/header.js
+++ scripts/Index/header.js
@@ -1,15 +1,22 @@
-function MudarOpacidadeElemento (elemento, opacit, blockDIV, opacitBlockDIV){
+function MudarOpacidadeElemento (elemento, opacit, blockDIV, opacitBlockDIV, entradaTrue){
     elemento.style.opacity = opacit;
     elemento.style.transition = "opacity 0.7s ease";
 
-
-    blockDIV.style.opacity = opacitBlockDIV
-    blockDIV.style.transition = "opacity 0.7s ease";
+    if (entradaTrue){
+        blockDIV.style.position = "fixed";
+        blockDIV.style.opacity = opacitBlockDIV;
+        blockDIV.style.transition = "opacity 0.7s ease";
+    }
+    else{
+        blockDIV.style.opacity = opacitBlockDIV;
+        blockDIV.style.transition = "opacity 0.7s ease";
+        setTimeout(() => {blockDIV.style.position = "relative";}, 500)
+    }
 }
 
 const botaoInterrogacao = document.getElementById('header-circle-question');
 const tolltipDIV = document.querySelector('.tolltip-header-question');
 const blockDIV = document.querySelector('.block-moment-tolltip')
 
-botaoInterrogacao.addEventListener('mouseover', () => MudarOpacidadeElemento(tolltipDIV, "100%", blockDIV, "80%"));
-botaoInterrogacao.addEventListener('mouseout', () => MudarOpacidadeElemento(tolltipDIV, "0%", blockDIV, "0%"));
+botaoInterrogacao.addEventListener('mouseover', () => MudarOpacidadeElemento(tolltipDIV, "100%", blockDIV, "80%", true));
+botaoInterrogacao.addEventListener('mouseout', () => MudarOpacidadeElemento(tolltipDIV, "0%", blockDIV, "0%", false));
