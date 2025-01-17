cc32c98271f8fcc13b897443c465ab72360480bc
Israel Bonicenha <israelbonicenha2020@gmail.com>
2025-01-17 11:11:26 -0300

    feat: adiciona animação na seta do btn-exibir-resto

--- scripts/Login/events.js
+++ scripts/Login/events.js
@@ -1,4 +1,4 @@
-function ExibirOcultar_restoRegistro(elemento) {
+function ExibirOcultar_restoRegistro(elemento, elemento2) {
 
     // Verificar a visibilidade do elemento
     const estilos = getComputedStyle(elemento)
@@ -7,15 +7,22 @@ function ExibirOcultar_restoRegistro(elemento) {
     if (éVisivel){
         elemento.style.left = "0px";
         elemento.style.transition = "left 1s ease";
+
+        elemento2.style.transform = "rotate(-0deg)";
+        elemento2.style.transition = "transform 1s ease";
     }
     else{
         elemento.style.left = "450px";
         elemento.style.transition = "left 1s ease";
+
+        elemento2.style.transform = "rotate(540deg)";
+        elemento2.style.transition = "transform 1s ease";
     }
 
 }
 
 const btnExibir_restoRegistro = document.getElementById("btn-exibir-resto")
 const restoDIVleft = document.querySelector(".resto-content-left")
+const setinhabtn = document.getElementById("setinhabtn")
 
-btnExibir_restoRegistro.addEventListener('click', () => ExibirOcultar_restoRegistro(restoDIVleft))
+btnExibir_restoRegistro.addEventListener('click', () => ExibirOcultar_restoRegistro(restoDIVleft, setinhabtn))
